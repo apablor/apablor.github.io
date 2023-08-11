4189f17924ecab3520e8925023c31b254236fd8c
apablor <apablor@calstatela.edu>
2023-08-11 01:41:04 -0700

    clean up

--- src/App.js
+++ src/App.js
@@ -4,15 +4,11 @@ import Loading from "react-loading";
 import './App.css';
 import NavBar from "./components/NavBar"; 
 import Banner from "./components/Banner"; 
-// import Skills from "./components/Skills"; 
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects"; 
 import Resume from "./components/Resume";
 import ContactMe from "./components/ContactMe"; 
 import Footer from "./components/Footer"; 
-import "./assets/CoestralPersonalUse-z8ZEw.woff"
-import "./assets/SunsetImaginaryPersonalUse-ALMyD.woff"
-
 // import 'bootstrap/dist/css/bootstrap.css';
 
 
